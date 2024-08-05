daaf1b6270379629ced52717d97b1d87b784d840
reoyan2525 <145842020+reoyan2525@users.noreply.github.com>
2024-08-05 14:38:48 +0900

    Add files via upload

---
+++ 41_ao_debit/link_footer.php
@@ -0,0 +1,56 @@
+<html>
+<meta http-equiv="refresh" content="0;url=https://af-110.com/pg.php?pgid=2137&adid=15212&j=listing&mid=14526&p=debit"><html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+          integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/slidebars.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.lightbox.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/mobile.css" media="only screen and (max-width:991px)">
+
+   <![endif]-->
+    <meta http-equiv="content-type" charset="utf-8">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131243579-6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-131243579-6');
+</script>
+
+<!-- Global site tag (gtag.js) - Google Ads: 687699712 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-687699712"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-687699712');
+</script>
+
+<!-- Event snippet for あおぞらビジネスデビット訴求遷移（MCV） conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-687699712/KyJPCNuKx9EBEIDu9ccC'});
+</script>
+
+
+<p>GMOあおぞらネット銀行 公式サイトへ移動します<br>
+<a href="https://af-110.com/pg.php?pgid=2137&adid=15212&j=listing&mid=14526&p=debit" target="_blank" rel="nofollow">
+https://af-110.com/pg.php?pgid=2137&adid=15212&j=listing&mid=14526</a>
+</p>
+
+<p>
+    中止する場合は<a href="/">こちら</a>をクリック
+</p>
+
+
+</body>
+</html>
